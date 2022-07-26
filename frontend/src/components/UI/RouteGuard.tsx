57ff716708fac696a3feda2361aaaebce3818202
LeonardoTrapani <leonard.trapani@gmail.com>
2022-07-26 23:54:56 +0200

    implemented login after refresh

--- frontend/src/components/UI/RouteGuard.tsx
+++ frontend/src/components/UI/RouteGuard.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
+import { authActions } from '../../store/auth-slice';
 
-import { useAppSelector } from '../../utilities/hooks';
+import { useAppDispatch, useAppSelector } from '../../utilities/hooks';
 
 const RouteGuard: React.FC<{ children: JSX.Element }> = (props) => {
   const isAuthenticated = useAppSelector((state) => state.auth.isAuthenticated);
-
-  if (!isAuthenticated) {
-    return <Navigate to='/login' replace />;
+  const dispatch = useAppDispatch();
+  if (isAuthenticated) {
+    return props.children;
   } else {
+    const token = localStorage.getItem('token');
+    if (token) {
+      dispatch(authActions.setToken(token));
+      return props.children;
+    } else {
+      return <Navigate to='/login' replace />;
+    }
     return props.children;
   }
 };
