f537f2593ee0a9779cb6ee59b94b499ed04d1d3a
LeonardoTrapani <leonard.trapani@gmail.com>
2022-08-02 16:34:15 +0200

    final fix to calculateFreeDays

--- backend/src/controllers/homework.ts
+++ backend/src/controllers/homework.ts
@@ -90,32 +90,14 @@ export const calculateFreeDays = async (
   res: Response,
   next: NextFunction
 ) => {
-  const { expirationDate, duration: homeworkDuration } = req.body;
+  const { expirationDate: expirationDateBody, duration: homeworkDuration } =
+    req.body;
   const { pageNumber } = req.params;
-  const expirationDateDate = new Date(expirationDate);
-
-  const startDate = addDaysFromToday(
-    +pageNumber * DAYS_PER_PAGE - DAYS_PER_PAGE
-  );
-  const endDate = addDays(startDate, DAYS_PER_PAGE);
+  const expirationDate = new Date(expirationDateBody);
 
   const { userId } = req;
   try {
-    const week = await prisma.week.findUnique({
-      where: {
-        userId: +userId!,
-      },
-      select: {
-        id: true,
-        mondayFreeMinutes: true,
-        tuesdayFreeMinutes: true,
-        wednesdayFreeMinutes: true,
-        thursdayFreeMinutes: true,
-        fridayFreeMinutes: true,
-        saturdayFreeMinutes: true,
-        sundayFreeMinutes: true,
-      },
-    });
+    const week = await fetchWeek(+userId!);
     if (!week) {
       return throwResponseError(
         'please define your usual week before creating any homework',
@@ -123,25 +105,7 @@ export const calculateFreeDays = async (
         res
       );
     }
-
-    const freeDays = await prisma.user.findUnique({
-      where: {
-        id: +userId!,
-      },
-      select: {
-        days: {
-          select: {
-            date: true,
-            freeMinutes: true,
-          },
-          where: {
-            date: {
-              gte: new Date(),
-            },
-          },
-        },
-      },
-    });
+    const freeDays = await fetchFreeDays(+userId!);
 
     if (!freeDays) {
       return throwResponseError(
@@ -151,10 +115,16 @@ export const calculateFreeDays = async (
       );
     }
 
+    const daysFromToday = +pageNumber * DAYS_PER_PAGE - DAYS_PER_PAGE;
+
+    const daysFromTodayWithSubtractedDays =
+      daysFromToday +
+      calculateSubtractedDays(+pageNumber, week, freeDays, homeworkDuration);
+    const startDate = addDaysFromToday(daysFromTodayWithSubtractedDays);
+
     const freeDaysArray = getFreeDaysArray(
       startDate,
-      endDate,
-      expirationDateDate,
+      expirationDate,
       week,
       freeDays,
       homeworkDuration
@@ -187,9 +157,46 @@ interface freeDays {
     freeMinutes: number;
   }[];
 }
+
+const fetchWeek = async (userId: number) => {
+  return await prisma.week.findUnique({
+    where: {
+      userId: userId,
+    },
+    select: {
+      id: true,
+      mondayFreeMinutes: true,
+      tuesdayFreeMinutes: true,
+      wednesdayFreeMinutes: true,
+      thursdayFreeMinutes: true,
+      fridayFreeMinutes: true,
+      saturdayFreeMinutes: true,
+      sundayFreeMinutes: true,
+    },
+  });
+};
+const fetchFreeDays = async (userId: number) => {
+  return await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+    select: {
+      days: {
+        select: {
+          date: true,
+          freeMinutes: true,
+        },
+        where: {
+          date: {
+            gte: new Date(),
+          },
+        },
+      },
+    },
+  });
+};
 const getFreeDaysArray = (
   startDate: Date,
-  endDate: Date,
   expirationDate: Date,
   week: week,
   freeDays: freeDays,
@@ -205,18 +212,24 @@ const getFreeDaysArray = (
     const freeDayToPut = freeDays.days.find((day) => {
       return day.date.toDateString() === currentDate.toDateString();
     });
-    if (freeDayToPut) {
-      if (freeDayToPut.freeMinutes >= homeworkDuration) {
+    const isDayValid = calculateIsDayValid(
+      freeDayToPut,
+      homeworkDuration,
+      freeMinutes
+    );
+    if (isDayValid) {
+      if (freeDayToPut) {
         finalFreeDays.push(freeDayToPut);
+      } else {
+        finalFreeDays.push({
+          date: currentDate,
+          freeMinutes,
+        });
       }
-    } else if (freeMinutes >= homeworkDuration) {
-      finalFreeDays.push({
-        date: currentDate,
-        freeMinutes,
-      });
     }
     currentDate = addDays(currentDate, 1);
   }
+
   return finalFreeDays;
 };
 
@@ -268,4 +281,59 @@ const addDays = (from: Date, daysToAdd: number) => {
   let result = new Date(from);
   result.setDate(result.getDate() + daysToAdd);
   return result;
+};
+
+const calculateSubtractedDays = (
+  pageNumber: number,
+  week: week,
+  freeDays: freeDays,
+  homeworkDuration: number
+) => {
+  if (pageNumber === 1) {
+    return 0;
+  }
+  const maxLength = pageNumber * DAYS_PER_PAGE - DAYS_PER_PAGE;
+  let currentDate = new Date();
+  let daysToSkip = 0;
+  let length = 0;
+  while (length < maxLength) {
+    const freeMinutes = findfreeMinutesInDay(currentDate, week);
+    const freeDayToPut = freeDays.days.find((day) => {
+      return day.date.toDateString() === currentDate.toDateString();
+    });
+    const isDayValid = calculateIsDayValid(
+      freeDayToPut,
+      homeworkDuration,
+      freeMinutes
+    );
+    if (isDayValid) {
+      length++;
+    } else {
+      daysToSkip++;
+    }
+    currentDate = addDays(currentDate, 1);
+  }
+
+  return daysToSkip;
+};
+
+const calculateIsDayValid = (
+  freeDayToPut:
+    | {
+        date: Date;
+        freeMinutes: number;
+      }
+    | undefined,
+  homeworkDuration: number,
+  freeMinutes: number
+) => {
+  if (freeDayToPut) {
+    if (freeDayToPut.freeMinutes >= homeworkDuration) {
+      return true;
+    }
+  } else if (freeMinutes >= homeworkDuration) {
+    return true;
+  }
+
+  return false;
 };
