c47acf1005832f3997328ec944fb41265b9047ad
LeonardoTrapani <leonard.trapani@gmail.com>
2022-08-16 14:40:46 +0200

    clened createWeekScreen

--- mobile/src/screens/CreateWeekScreen.tsx
+++ mobile/src/screens/CreateWeekScreen.tsx
@@ -1,68 +1,10 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import MyTimePicker, { useTimePicker } from '../components/MyTimePicker';
 import { BoldText, MediumText } from '../components/StyledText';
-import TextButton from '../components/TextButton';
 import WeekDuration from '../components/WeekDuration';
 
 const CreateWeekScreen = () => {
-  const {
-    isOpened: mondayIsOpened,
-    onCancel: mondayOnCancel,
-    onConfirm: mondayOnConfirm,
-    open: mondayOpen,
-    minutes: mondayMinutes,
-  } = useTimePicker();
-
-  const {
-    isOpened: tuesdayIsOpened,
-    onCancel: tuesdayOnCancel,
-    onConfirm: tuesdayOnConfirm,
-    open: tuesdayOpen,
-    minutes: tuesdayMinutes,
-  } = useTimePicker();
-
-  const {
-    isOpened: wednesdayIsOpened,
-    onCancel: wednesdayOnCancel,
-    onConfirm: wednesdayOnConfirm,
-    open: wednesdayOpen,
-    minutes: wednesdayMinutes,
-  } = useTimePicker();
-
-  const {
-    isOpened: thursdayIsOpened,
-    onCancel: thursdayOnCancel,
-    onConfirm: thursdayOnConfirm,
-    open: thursdayOpen,
-    minutes: thursdayMinutes,
-  } = useTimePicker();
-
-  const {
-    isOpened: fridayIsOpened,
-    onCancel: fridayOnCancel,
-    onConfirm: fridayOnConfirm,
-    open: fridayOpen,
-    minutes: fridayMinutes,
-  } = useTimePicker();
-
-  const {
-    isOpened: saturdayIsOpened,
-    onCancel: saturdayOnCancel,
-    onConfirm: saturdayOnConfirm,
-    open: saturdayOpen,
-    minutes: saturdayMinutes,
-  } = useTimePicker();
-
-  const {
-    isOpened: sundayIsOpened,
-    onCancel: sundayOnCancel,
-    onConfirm: sundayOnConfirm,
-    open: sundayOpen,
-    minutes: sundayMinutes,
-  } = useTimePicker();
-
   return (
     <SafeAreaView>
       <MediumText style={styles.oneMoreStepToGo}>
