e9227106ed2313e115b9e813bf131892be44cffc
LeonardoTrapani <leonard.trapani@gmail.com>
2022-08-17 19:30:14 +0200

    floatingactionbutton on main screen

--- mobile/src/constants/Syles.ts
+++ mobile/src/constants/Syles.ts
@@ -23,6 +23,17 @@ const globalStyles = StyleSheet.create({
 
     elevation: 3,
   },
+  bigShadow: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.36,
+    shadowRadius: 6.68,
+
+    elevation: 11,
+  },
 });
 
 export default globalStyles;
