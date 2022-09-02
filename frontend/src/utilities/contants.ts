909d333f2b6179bab594cc5b05670a28f5e9b530
LeonardoTrapani <leonard.trapani@gmail.com>
2022-09-02 21:55:49 +0200

    reverted recursive functiong

--- frontend/src/utilities/contants.ts
+++ frontend/src/utilities/contants.ts
@@ -1,4 +1,4 @@
-export const BACKEND_URL = 'http://localhost:3000';
+export const BACKEND_URL = "http://localhost:3000";
 
 export const SHOW_BURGER_MENU_PX = 768;
 
@@ -8,28 +8,28 @@ interface NavigationItem {
 }
 export const NAVIGATION_ITEMS: NavigationItem[] = [
   {
-    name: 'home',
-    to: '/',
+    name: "home",
+    to: "/",
   },
   {
-    name: 'Add homework',
-    to: '/create-homework',
+    name: "Add homework",
+    to: "/create-homework",
   },
 ];
 
 export const MONTHS = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December',
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
 ];
 
 export const BAR_OPACITY = 0.5;
