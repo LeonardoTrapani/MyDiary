0c7677eafcef941d0a150c09c500c60c8b4a6fa8
LeonardoTrapani <leonard.trapani@gmail.com>
2022-10-01 19:22:19 +0200

    removed userId relation to grade table - still not working with multiple grades on a single subject

--- backend/src/controllers/grade.ts
+++ backend/src/controllers/grade.ts
@@ -5,17 +5,27 @@ import { throwResponseError } from "../utilities";
 export const createGrade = async (req: Request, res: Response) => {
   const { grade, subjectId } = req.body;
   const { userId } = req;
+
+  const validSubjects = await prisma.subject.findMany({
+    where: {
+      userId: +userId!,
+    },
+  });
+
+  if (!validSubjects.length) {
+    throwResponseError("this subject was not created by you", 400, res);
+    return;
+  }
   try {
     const gradeResult = await prisma.grade.create({
       data: {
         grade: +grade,
-        userId: +userId!,
         subjectId: +subjectId,
       },
     });
     res.json(gradeResult);
   } catch (err) {
-    console.log(err);
+    console.error(err);
     throwResponseError("an error has occurred creating the grade", 400, res);
   }
 };
@@ -41,7 +51,6 @@ export const getAllGrades = async (req: Request, res: Response) => {
                 grade: true,
               },
               where: {
-                userId: +userId!,
                 deleted: false,
               },
             },
