471b504aecdf83728d77dac060b6338ec7a7f00a
LeonardoTrapani <leonard.trapani@gmail.com>
2022-11-08 13:19:56 +0100

    completed time transaction

--- backend/src/controllers/plannedDates.ts
+++ backend/src/controllers/plannedDates.ts
@@ -5,68 +5,70 @@ import { prisma } from "../app";
 export const completePlannedDate = async (req: Request, res: Response) => {
   const { id } = req.params;
 
-  const previousPlannedDate = await prisma.plannedDate.findUnique({
-    where: { id: +id },
-    select: {
-      minutesAssigned: true,
-      homework: {
-        select: {
-          timeToComplete: true,
+  await prisma.$transaction(async (trx) => {
+    const previousPlannedDate = await trx.plannedDate.findUnique({
+      where: { id: +id },
+      select: {
+        minutesAssigned: true,
+        homework: {
+          select: {
+            timeToComplete: true,
+          },
         },
       },
-    },
-  });
+    });
 
-  if (!previousPlannedDate) {
-    throwResponseError("planned date doesnt exist", 400, res);
-    return;
-  }
+    if (!previousPlannedDate) {
+      throwResponseError("planned date doesnt exist", 400, res);
+      return;
+    }
 
-  await prisma.plannedDate.update({
-    where: {
-      id: +id,
-    },
-    data: {
-      completed: true,
-    },
-  });
+    await trx.plannedDate.update({
+      where: {
+        id: +id,
+      },
+      data: {
+        completed: true,
+      },
+    });
 
-  const previousMinutesAssigned = previousPlannedDate.minutesAssigned;
+    const previousMinutesAssigned = previousPlannedDate.minutesAssigned;
 
-  await prisma.homework.updateMany({
-    where: {
-      plannedDates: {
-        some: {
-          id: +id,
+    await trx.homework.updateMany({
+      where: {
+        plannedDates: {
+          some: {
+            id: +id,
+          },
         },
       },
-    },
-    data: {
-      timeToComplete: {
-        decrement: previousMinutesAssigned,
-      },
-    },
-  });
-
-  //if all the time is completed set it to 0
-  await prisma.homework.updateMany({
-    where: {
-      plannedDates: {
-        some: {
-          id: +id,
+      data: {
+        timeToComplete: {
+          decrement: previousMinutesAssigned,
         },
       },
-      timeToComplete: 0,
-    },
-    data: {
-      completed: true,
-    },
-  });
+    });
 
-  if (!previousPlannedDate) {
-    throwResponseError("Planned date not existing", 400, res);
-    return;
-  }
+    //if all the time is completed set it to 0
+    await trx.homework.updateMany({
+      where: {
+        plannedDates: {
+          some: {
+            id: +id,
+          },
+        },
+        timeToComplete: 0,
+      },
+      data: {
+        completed: true,
+      },
+    });
+
+    if (!previousPlannedDate) {
+      throwResponseError("Planned date not existing", 400, res);
+      return;
+    }
+  });
   res.json("success");
   return;
 };
@@ -74,49 +76,51 @@ export const completePlannedDate = async (req: Request, res: Response) => {
 export const uncompletePlannedDate = async (req: Request, res: Response) => {
   const { id } = req.params;
 
-  const previousPlannedDate = await prisma.plannedDate.findUnique({
-    where: { id: +id },
-    select: {
-      minutesAssigned: true,
-    },
-  });
+  await prisma.$transaction(async (trx) => {
+    const previousPlannedDate = await trx.plannedDate.findUnique({
+      where: { id: +id },
+      select: {
+        minutesAssigned: true,
+      },
+    });
 
-  if (!previousPlannedDate) {
-    throwResponseError("planned date doesnt exist", 400, res);
-    return;
-  }
+    if (!previousPlannedDate) {
+      throwResponseError("planned date doesnt exist", 400, res);
+      return;
+    }
 
-  await prisma.plannedDate.update({
-    where: {
-      id: +id,
-    },
-    data: {
-      completed: false,
-    },
-  });
+    await trx.plannedDate.update({
+      where: {
+        id: +id,
+      },
+      data: {
+        completed: false,
+      },
+    });
 
-  const previousMinutesAssigned = previousPlannedDate.minutesAssigned;
+    const previousMinutesAssigned = previousPlannedDate.minutesAssigned;
 
-  await prisma.homework.updateMany({
-    where: {
-      plannedDates: {
-        some: {
-          id: +id,
+    await trx.homework.updateMany({
+      where: {
+        plannedDates: {
+          some: {
+            id: +id,
+          },
         },
       },
-    },
-    data: {
-      completed: false,
-      timeToComplete: {
-        increment: previousMinutesAssigned,
+      data: {
+        completed: false,
+        timeToComplete: {
+          increment: previousMinutesAssigned,
+        },
       },
-    },
-  });
+    });
 
-  if (!previousPlannedDate) {
-    throwResponseError("Planned date not existing", 400, res);
-    return;
-  }
+    if (!previousPlannedDate) {
+      throwResponseError("Planned date not existing", 400, res);
+      return;
+    }
+  });
   res.json("success");
   return;
 };
