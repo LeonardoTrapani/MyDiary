e54f5f7625eb98aa6d000e9fe7047ee09d5dd0bc
LeonardoTrapani <leonard.trapani@gmail.com>
2022-12-24 20:12:11 +0100

    PLANNED_DATES_PER_PAGE

--- mobile/src/constants/constants.ts
+++ mobile/src/constants/constants.ts
@@ -1,4 +1,3 @@
-//export const BACKEND_URL = "mydiary-production.up.railway.app";
 export const BACKEND_URL = "http://localhost:3000";
-export const PLANNED_DATES_PER_PAGE = 10;
+export const PLANNED_DATES_PER_PAGE = 5;
 export const MINIMUM_HOMEWORK_HEIGHT = 30;
