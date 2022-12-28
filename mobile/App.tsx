e0e01f4d8fe25097275f55eace1e4562216eb232
LeonardoTrapani <leonard.trapani@gmail.com>
2022-12-28 13:06:26 +0100

    fixed timezone problems

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -3,6 +3,9 @@ import { useColorScheme } from "react-native";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Main from "./src/navigation";
+import momentTz from "moment-timezone";
+
+momentTz.tz.setDefault("America/New_York");
 
 const App = () => {
   const colorScheme = useColorScheme();
