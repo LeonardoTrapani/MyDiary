bf7ce04f76d4fb1c1014fed1f76de95ef71b0566
LeonardoTrapani <leonard.trapani@gmail.com>
2023-01-01 12:27:16 +0100

    settings screen initialization

--- mobile/src/screens/SettingsScreen.tsx
+++ mobile/src/screens/SettingsScreen.tsx
@@ -1,9 +1,12 @@
+import { Ionicons } from "@expo/vector-icons";
+import { useTheme } from "@react-navigation/native";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import React from "react";
-import { StyleSheet } from "react-native";
+import { ScrollView, StyleSheet, TouchableOpacity } from "react-native";
 import { logout } from "../api/auth";
+import { RegularText } from "../components/StyledText";
 import TextButton from "../components/TextButton";
-import { View } from "../components/Themed";
+import { CardView, View } from "../components/Themed";
 
 const SettingsScreen: React.FC = () => {
   const queryClient = useQueryClient();
@@ -21,15 +24,73 @@ const SettingsScreen: React.FC = () => {
 
   return (
     <View style={styles.container}>
-      <TextButton title="logout" onPress={() => logoutMutation.mutate()} />
+      <ScrollView>
+        <SettingsCard
+          onPress={() => {
+            console.log("account");
+          }}
+          name="Account"
+          iconName="person"
+        />
+        <SettingsCard
+          onPress={() => {
+            console.log("account");
+          }}
+          name="Week"
+          iconName="calendar"
+        />
+        <SettingsCard
+          onPress={() => {
+            console.log("account");
+          }}
+          name="Subjects"
+          iconName="flag"
+        />
+        <SettingsCard
+          onPress={() => {
+            console.log("account");
+          }}
+          name="Professors"
+          iconName="barcode"
+        />
+        <TextButton title="logout" onPress={() => logoutMutation.mutate()} />
+      </ScrollView>
     </View>
+  );
+};
+
+const SettingsCard: React.FC<{
+  onPress: () => void;
+  name: string;
+  iconName: keyof typeof Ionicons.glyphMap;
+}> = (props) => {
+  const { text } = useTheme().colors;
+  return (
+    <TouchableOpacity onPress={props.onPress}>
+      <CardView
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          borderBottomWidth: 0.5,
+          borderBottomColor: "#ddd",
+          padding: 15,
+        }}
+      >
+        <RegularText style={{ fontSize: 18 }}>{props.name}</RegularText>
+        <Ionicons
+          name={props.iconName}
+          size={18}
+          color={text}
+          style={{ opacity: 0.8 }}
+        />
+      </CardView>
+    </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
   },
 });
 
